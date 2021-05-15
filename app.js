7d8ccd0fd71a2b4476b50db8ddb4eba1f03c8d10
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-05-15 20:23:20 +0530

    updated JS file

---
+++ app.js
@@ -0,0 +1,23 @@
+var userInput = document.querySelector(".txt_input")
+var translateInput = document.querySelector(".translate_txt")
+var displayOutput = document.querySelector(".txt_output")
+
+var url = "https://api.funtranslations.com/yaml/funtranslations.yaml"
+
+function generateUrl(text) {
+    return url+"?text="+text
+}
+
+function errorHandler(error) {
+    alert("Somethimg went wrong. Try after some time.")
+}
+
+function clickResponse(text) {
+
+    fetch(generateUrl(text)).then(response=>response.json).then(json=>{
+        var translatedText = json.contents.translated
+        displayOutput.innerText = translatedText
+    })
+}
+
+translateInput.addEventListener("click",clickResponse)
